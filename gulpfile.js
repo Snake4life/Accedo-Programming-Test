50f0567e0d47964700baa48c01770e420cb87972
Hermann Yung <hermannyung@gmail.com>
2015-10-06 00:06:36 +0800

    Add less

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    less = require("gulp-less"),
 	watch = require('gulp-watch'),
     jshint = require('gulp-jshint'),
     webpack = require('gulp-webpack'),
@@ -11,6 +12,13 @@ gulp.task('lint', function  () {
         .pipe(jshint.reporter('default'))
 })
 
+gulp.task("less", function(){
+    gulp.src("src/less/entry.less")
+        .pipe(less())
+        .pipe(rename("main.css"))
+        .pipe(gulp.dest("dist/css"))
+})
+
 gulp.task('webpack', function () {
     gulp.src('src/js/entry.js')
         .pipe(webpack())
@@ -21,6 +29,6 @@ gulp.task('webpack', function () {
 })
 
 gulp.task('default', function(){
-    gulp.run('lint', 'webpack')
-    gulp.watch(["src/**/*.js"], {interval: 1000}, ["lint", "webpack"])
+    gulp.run('lint', 'webpack', 'less')
+    gulp.watch(["src/**/*.js"], {interval: 1000}, ["lint", "webpack", "less"])
 })
