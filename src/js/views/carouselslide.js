a6f84ea8e9a06463b9f2cfca40f769bf61fe8a35
Hermann Yung <hermannyung@gmail.com>
2015-10-08 19:59:25 +0800

    Add enter key function

--- src/js/views/carouselslide.js
+++ src/js/views/carouselslide.js
@@ -3,21 +3,27 @@ var globalEvents = require("../global-events")
 module.exports = Backbone.View.extend({
 	className: "item",
 	events: {
-		"click img": function(){
-			globalEvents.trigger("itemwatch", this.model.get("id"))
-			this.$video[0].webkitRequestFullscreen()
-			this.$video[0].play()
-
-			var self = this
-
-			$(document).one("webkitfullscreenchange", function(){
-				$(document).one("webkitfullscreenchange", function(){
-					if(!document.webkitFullscreenElement){
-						self.$video[0].pause()
-					}
-				})
-			})
+		"click img": "playVideo",
+		"keyup img": function(e){
+			if (e.keyCode == 13){
+				e.currentTarget.click()
+			}
 		}
+	},
+	playVideo: function(){
+		globalEvents.trigger("itemwatch", this.model.get("id"))
+		this.$video[0].webkitRequestFullscreen()
+		this.$video[0].play()
+
+		var self = this
+
+		$(document).one("webkitfullscreenchange", function(){
+			$(document).one("webkitfullscreenchange", function(){
+				if(!document.webkitFullscreenElement){
+					self.$video[0].pause()
+				}
+			})
+		})
 	},
 	initialize: function(options){
 		this.options = options
@@ -26,7 +32,7 @@ module.exports = Backbone.View.extend({
 		var src = this.model.get("images")[0].url
 		var caption = this.model.get("title")
 
-		var $image = $("<img>").attr("src", src)
+		var $image = $("<img>").attr({src: src, tabIndex: 1})
 		var $caption = $("<div>").addClass("carousel-caption").html(caption)
 
 		this.$video = $("<video>").attr({
